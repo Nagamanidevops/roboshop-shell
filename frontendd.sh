855323350a3f491d201e707a3b64283e41fa3f14
nagamani <nagamanichavaaws@gmail.com>
2023-01-04 10:15:37 +0000

    added frontendd file

---
+++ frontendd.sh
@@ -0,0 +1,18 @@
+yum install nginx -y 
+
+systemctl enable nginx 
+
+systemctl start nginx 
+
+rm -rf /usr/share/nginx/html/* 
+
+curl -o /tmp/frontend.zip https://roboshop-artifacts.s3.amazonaws.com/frontend.zip 
+ur
+cd /usr/share/nginx/html 
+
+unzip /tmp/frontend.zip
+
+
+cp files/nginx-roboshop.conf /etc/nginx/default.d/roboshop.conf
+
+systemctl restart nginx 
