79d5da488fd035246ddde172105553073c89e2a5
nagamani <nagamanichavaaws@gmail.com>
2023-01-04 10:32:51 +0000

    added

--- frontendd.sh
+++ frontendd.sh
@@ -15,6 +15,6 @@ cd /usr/share/nginx/html
 unzip /tmp/frontend.zip
 
 echo $scriptLocation
-cp $(scriptLocation)files/nginx-roboshop.conf /etc/nginx/default.d/roboshop.conf
+cp ($scriptLocation)/files/nginx-roboshop.conf /etc/nginx/default.d/roboshop.conf
 
 systemctl restart nginx 
