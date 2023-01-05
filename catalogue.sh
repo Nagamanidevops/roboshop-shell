aeaf0fc0881034d711929eec99ff100ba5f39abf
nagamani <nagamanichavaaws@gmail.com>
2023-01-05 07:08:26 +0000

    addd host

--- catalogue.sh
+++ catalogue.sh
@@ -27,3 +27,4 @@ systemctl start catalogue
 cp ${scriptLocation}/files/mongodb.repo /etc/yum.repos.d/mongodb.repo
 yum install mongodb-org-shell -y
 
+mongo --host localhost </app/schema/catalogue.js
