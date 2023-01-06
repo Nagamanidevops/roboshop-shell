777bad467c99fb6273291e702c526e4a028dc1b3
nagamani <nagamanichavaaws@gmail.com>
2023-01-06 04:41:13 +0000

    added log file

--- frontendd.sh
+++ frontendd.sh
@@ -1,22 +1,24 @@
 scriptLocation=$(pwd)
+
+LOG=/tmp/roboshop.log
 echo -e "\e[35m install nginx\e[0m"
-yum install nginx -y 
+yum install nginx -y &>>${LOG}
 
 
 
-rm -rf /usr/share/nginx/html/* 
+rm -rf /usr/share/nginx/html/* &>>${log}
 
-curl -o /tmp/frontend.zip https://roboshop-artifacts.s3.amazonaws.com/frontend.zip 
+curl -o /tmp/frontend.zip https://roboshop-artifacts.s3.amazonaws.com/frontend.zip &>>${LOG}
 ur
-cd /usr/share/nginx/html 
+cd /usr/share/nginx/html &>>${LOG}
 
 unzip /tmp/frontend.zip
 
 echo $scriptLocation
 
-cp ${scriptLocation}/files/nginx-roboshop.conf /etc/nginx/default.d/roboshop.conf
+cp ${scriptLocation}/files/nginx-roboshop.conf /etc/nginx/default.d/roboshop.conf &>>${LOG}
 
  
-systemctl enable nginx 
+systemctl enable nginx &>>${LOG}
 
-systemctl restart nginx 
+systemctl restart nginx &>>${LOG}
