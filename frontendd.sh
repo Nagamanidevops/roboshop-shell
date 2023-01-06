7458a92c3093d838f02bf014ab0744ddfa683167
nagamani <nagamanichavaaws@gmail.com>
2023-01-06 08:42:40 +0000

    added if else

--- frontendd.sh
+++ frontendd.sh
@@ -12,27 +12,51 @@ fi
 
 echo -e "\e[31m remove nginx old content\e[0m"
 rm -rf /usr/share/nginx/html/* 
-echo $?
+if [ $? -eq 0 ]; then
+echo success
+else
+echo failed
+fi
 
 echo -e "\e[32mdownload frontend content \e[0m"
 curl -o /tmp/frontend.zip https://roboshop-artifacts.s3.amazonaws.com/frontend.zip &>>${LOG}
-echo $?
+if [ $? -eq 0 ]; then
+echo success
+else
+echo failed
+fi
 
 cd /usr/share/nginx/html &>>${LOG}
 
 echo -e "\e[33mextract frontend content \e[0m"
 unzip /tmp/frontend.zip &>>{LOG}
-echo $?
+if [ $? -eq 0 ]; then
+echo success
+else
+echo failed
+fi
 
 echo $scriptLocation
 echo -e "\e[34mcopy nginx roboshop config file\e[0m"
 cp ${scriptLocation}/files/nginx-roboshop.conf /etc/nginx/default.d/roboshop.conf &>>${LOG}
-echo $?
+if [ $? -eq 0 ]; then
+echo success
+else
+echo failed
+fi
 echo -e "\e[36m ENABLE nginx\e[0m" 
 systemctl enable nginx &>>${LOG}
-echo $?
+if [ $? -eq 0 ]; then
+echo success
+else
+echo failed
+fi
 
 echo -e "\e[36m start nginx\e[0m"
 
 systemctl restart nginx &>>${LOG}
-echo $?
+if [ $? -eq 0 ]; then
+echo success
+else
+echo failed
+fi
