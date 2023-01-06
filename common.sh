cb078261fd27a3beade5bd69bdaad74f447cc119
nagamani <nagamanichavaaws@gmail.com>
2023-01-06 10:34:45 +0000

    added head

--- common.sh
+++ common.sh
@@ -3,10 +3,15 @@ LOG=/tmp/roboshop.log
 
 status_check(){
  if [ $? -eq 0 ]; then
-  echo -e "\e[32success\e[0m"
+  echo -e "\e[1;32success\e[0m"
  else
-  echo -e "\e[31m failed js\e[0m"
+  echo -e "\e[1;31m failed js\e[0m"
   echo "refer log file , LOG - ${LOG}"
   exit
  fi
+}
+
+print_head()
+{
+    echo -e "\e[1m $1 \e[0m"
 }
