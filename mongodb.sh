f95fe036cdfd59167ce357b0518e35427c2a93ea
nagamani <nagamanichavaaws@gmail.com>
2023-01-07 04:43:39 +0000

    added comments in mongoDB

--- mongodb.sh
+++ mongodb.sh
@@ -1,10 +1,23 @@
 scriptLocation=$(pwd)
-cp ${scriptLocation}/files/mongodb.repo /etc/yum.repos.d/mongodb.repo
+source common.sh
 
-yum install mongodb-org -y 
- 
+print_head "copy momgodb repo file"
+cp ${scriptLocation}/files/mongodb.repo /etc/yum.repos.d/mongodb.repo &>>${LOG}
+status_check
 
- sed -i -e 's/127.0.0.1/0.0.0.0/' /etc/mongod.conf
+print_head "install MongoDB"
+yum install mongodb-org -y &>>${LOG}
+status_check
+
  
-systemctl enable mongod 
-systemctl restart mongod
+print_head "Update MongoDB listen adress"
+ sed -i -e 's/127.0.0.1/0.0.0.0/' /etc/mongod.conf &>>${LOG}
+ status_check
+ 
+print_head "install MongoDB"
+systemctl enable mongod &>>${LOG}
+status_check
+
+print_head " restart MongoDB"
+systemctl restart mongod &>>${LOG}
+status_check
