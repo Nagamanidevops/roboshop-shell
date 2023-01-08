9617ca74e5f5ac795c5031be6d770ef74f8d28a8
nagamani <nagamanichavaaws@gmail.com>
2023-01-08 09:56:59 +0000

    user

--- redis.sh
+++ redis.sh
@@ -1,17 +1,17 @@
 scriptLocation=$(pwd)
 source common.sh
 
-print_head "install remi repo file"
+print_head "setup redis repo"
 yum install https://rpms.remirepo.net/enterprise/remi-release-8.rpm -y &>>${LOG}
 status_check
 
 print_head "enable  redis 6.2 dnf module"
 dnf module enable redis:remi-6.2 -y &>>${LOG}
+status_check
 
 print_head "install redis"
 yum install redis -y &>>${LOG}
 status_check
-
  
 print_head "Update redis listen adress"
  sed -i -e 's/127.0.0.1/0.0.0.0/' /etc/redis.conf /etc/redis/redis.conf  &>>${LOG}
