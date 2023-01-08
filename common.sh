7e4a5a2994fdfaf75687321b2dfaafff0b1a3231
nagamani <nagamanichavaaws@gmail.com>
2023-01-08 07:40:57 +0000

    added dry code

--- common.sh
+++ common.sh
@@ -15,3 +15,80 @@ print_head()
 {
     echo -e "\e[1m $1 \e[0m"
 }
+
+
+NODEJS()
+{
+ print_head "configuring nodejs repos"
+curl -sL https://rpm.nodesource.com/setup_lts.x | bash &>>${LOG}
+status_check
+
+print_head "install Node j"
+yum install nodejs -y &>>${LOG}
+status_check
+
+print_head "add application use"
+id roboshop &>>${LOG}
+if [$? -ne 0] ; then
+useradd roboshop &>>${LOG}
+fi
+
+mkdir -p /app &>>${LOG}
+
+print_head "downloading app conten"
+curl -L -o /tmp/${component}.zip https://roboshop-artifacts.s3.amazonaws.com/${component}.zip &>>${LOG} 
+status_check
+
+ print_head "m cleanup old content"
+status_check
+
+rm -rf /app/*
+
+print_head "extracting app content"
+cd /app 
+unzip /tmp/${component}.zip &>>${LOG}
+status_check
+
+
+
+print_head " installing Node js dependencie"
+cd /app 
+npm install &>>${LOG}
+status_check
+
+
+
+print_head " installing Node js dependencie"
+cp ${scriptLocation}/files/${component}.service /etc/systemd/system/${component}.service &>>${LOG}
+status_check
+
+
+print_head "system d"
+systemctl daemon-reload &>>${LOG}
+status_check
+
+
+print_head "enable ${component} servic"
+systemctl enable ${component} &>>${LOG}
+status_check
+
+
+print_head"start ${component} servic"
+systemctl start ${component} &>>${LOG}
+status_check
+
+
+print_head "configuring mongo repo"
+cp ${scriptLocation}/files/mongodb.repo /etc/yum.repos.d/mongodb.repo &>>${LOG}
+status_check
+
+print_head "install mongo client"
+yum install mongodb-org-shell -y &>>${LOG}
+status_check
+
+print_head "load schema"
+mongo --host mongodb-dev.devopsg70.online  </app/schema/${component}.js &>>${LOG}
+status_check
+
+
+}
